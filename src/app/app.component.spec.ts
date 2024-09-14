4dbab2d845ab7bb778e35cbf022bdf5b4665e12e
mlklk-22 <mlkmsbh84@gmail.com>
2024-09-14 23:46:02 +0300

    Add Remiaing Structure Files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'VehicleWeb' title`, () => {
+  it(`should have the 'CarseerWeb' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('VehicleWeb');
+    expect(app.title).toEqual('CarseerWeb');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, VehicleWeb');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, CarseerWeb');
   });
 });
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { HttpClient, provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CarApiService } from './app.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+     provideRouter(routes), provideClientHydration(),
+     provideHttpClient(withInterceptorsFromDi(),withFetch()),
+    ]
 };
